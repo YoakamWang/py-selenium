01f4ae93f7dd042e3103fa1c665ddfab13fadfc0
YoakamWang <90314896+YoakamWang@users.noreply.github.com>
2022-10-25 11:47:51 +0800

    Add files via upload

--- py_web_sele.py
+++ py_web_sele.py
@@ -61,89 +61,85 @@ def newpage(driver, wait, home, parentnumber, cnumber, cname, ctype, csource, cr
     # time.sleep(2)
     wait.until(EC.visibility_of_element_located(
         (By.XPATH, "//*[@class='x-grid3-scroller']//span[contains(text(),'" + parentnumber + "')]")))
-    insert_new_button = driver.find_element(By.XPATH, "//button[contains(text(),'Insert New')]")
+    insert_new_button = wait.until(
+        EC.visibility_of_element_located((By.XPATH, "//button[contains(text(),'Insert New')]")))
     new_button_state = insert_new_button.is_enabled()
     # print("new" + str(new_button_state))
     if new_button_state == False:
         return
-    insert_new_button.click()
-    wait.until(
-        EC.number_of_windows_to_be(2)
-    )
-    tohandle = driver.window_handles  # CDwindow-BB52731DD9801849847E3F60C044733F
-    # print(toHandle)
-    for handle in tohandle:
-        if handle == home:
-            continue
-        driver.switch_to.window(handle)
-    driver.maximize_window()
-    # print(driver.title)
-    number = wait.until(EC.element_to_be_clickable((By.ID, 'number')))
-    number.send_keys(cnumber)
-    name = driver.find_element(By.XPATH, "//td[@attrid='name']/input[@type='text']")
-    name.send_keys(cname)
-    assemblymode = driver.find_element(By.ID, 'partType')
-    Select(assemblymode).select_by_value(ctype)
-    source = driver.find_element(By.ID, "source")
-    Select(source).select_by_value(csource)
-    reverison = driver.find_element(By.XPATH, "//*[@onchange='PTC.revision.validateRevision(event);']")
-    reverison.clear()
-    try:
-        wait.until(EC.alert_is_present())
-        driver.switch_to.alert.accept()
-    finally:
-        reverison.send_keys(creversion)
-    # time.sleep(1)
-    # tracecode = driver.find_element(By.ID, "defaultTraceCode")
-    # Select(tracecode).select_by_value("")
-    # defaultunit = driver.find_element(By.ID, "defaultUnit")
-    # Select(defaultunit).select_by_value("")
-    branditem = driver.find_element(By.ID, "BrndMrkItm")
-    Select(branditem).select_by_value("No marking")
-    accessory = driver.find_element(By.ID, "org.niladv.accessory")
-    Select(accessory).select_by_value(caccessory)
-    seperatepart = driver.find_element(By.ID, "org.niladv.SparePrt")
-    Select(seperatepart).select_by_value(csparepart)
+    else:
+        insert_new_button.click()
+        wait.until(
+            EC.number_of_windows_to_be(2)
+        )
+        tohandle = driver.window_handles  # CDwindow-BB52731DD9801849847E3F60C044733F
+        # print(toHandle)
+        for handle in tohandle:
+            if handle == home:
+                continue
+            driver.switch_to.window(handle)
+        driver.maximize_window()
+        number = wait.until(EC.element_to_be_clickable((By.ID, 'number')))
+        number.send_keys(cnumber)
+        name = driver.find_element(By.XPATH, "//td[@attrid='name']/input[@type='text']")
+        name.send_keys(cname)
+        assemblymode = driver.find_element(By.ID, 'partType')
+        Select(assemblymode).select_by_value(ctype)
+        source = driver.find_element(By.ID, "source")
+        Select(source).select_by_value(csource)
+        reverison = driver.find_element(By.XPATH, "//*[@onchange='PTC.revision.validateRevision(event);']")
+        reverison.clear()
+        try:
+            wait.until(EC.alert_is_present())
+            driver.switch_to.alert.accept()
+        finally:
+            reverison.send_keys(creversion)
+        branditem = driver.find_element(By.ID, "BrndMrkItm")
+        Select(branditem).select_by_value("No marking")
+        accessory = driver.find_element(By.ID, "org.niladv.accessory")
+        Select(accessory).select_by_value(caccessory)
+        seperatepart = driver.find_element(By.ID, "org.niladv.SparePrt")
+        Select(seperatepart).select_by_value(csparepart)
 
-    gatheringparts = driver.find_elements(By.XPATH, "//*[@id='hidePartInStructure']//label")
-    for part in gatheringparts:
-        if part.text == cgatheringpart:
-            part.click()
-    material = driver.find_element(By.ID, "MATERIAL")
-    material.location_once_scrolled_into_view
-    criticalcharas = driver.find_elements(By.XPATH, "//*[@id='org.niladv.CriticalChar']//label")
-    for chara in criticalcharas:
-        if chara.text == ccriticalcharac:
-            chara.click()
-    finish_button = driver.find_element(By.ID, "ext-gen39")
-    finish_button.click()
-    # time.sleep(10)
-    try:
-        wait.until(EC.alert_is_present())
-        driver.switch_to.alert.accept()
-        driver.find_element(By.ID, 'ext-gen41').click()
-        driver.switch_to.window(home)
-        wait.until(EC.visibility_of_element_located((By.ID, 'psbIFrame')))
-        #     # Switch to the frame //Switch to base frame  switchTo().defaultContent()
-        driver.switch_to.frame('psbIFrame')
-        existpage(driver, wait, parentnumber, cnumber, cqty, ctechpart)
-    except NoAlertPresentException:
-        driver.switch_to.window(home)
-        wait.until(EC.visibility_of_element_located((By.ID, 'psbIFrame')))
-        #     # Switch to the frame //Switch to base frame  switchTo().defaultContent()
-        driver.switch_to.frame('psbIFrame')
-        # edit_usage_page = driver.find_elements(By.XPATH, "//span[contains(text(),'Edit Usage Attributes')]")
-        qty_input_new = wait.until(EC.element_to_be_clickable((By.XPATH,
-                                                               "//label[contains(text(),'*Quantity')]/../../following-sibling::td[1]//input")))  # complex locate element xpath
-        qty_input_new.clear()
-        qty_input_new.send_keys(cqty)
-        time.sleep(1)
-        tech_new = driver.find_element(By.XPATH,
-                                       "//label[contains(text(),'*Technical')]/../../following-sibling::td[1]//input")
-        tech_new.clear()
-        tech_new.send_keys(ctechpart)
-        time.sleep(1)
-        driver.find_element(By.XPATH, "//button[contains(text(),'OK')]").click()
+        gatheringparts = driver.find_elements(By.XPATH, "//*[@id='hidePartInStructure']//label")
+        for part in gatheringparts:
+            if part.text == cgatheringpart:
+                part.click()
+        material = driver.find_element(By.ID, "MATERIAL")
+        material.location_once_scrolled_into_view
+        criticalcharas = driver.find_elements(By.XPATH, "//*[@id='org.niladv.CriticalChar']//label")
+        for chara in criticalcharas:
+            if chara.text == ccriticalcharac:
+                chara.click()
+        finish_button = driver.find_element(By.ID, "ext-gen39")
+        finish_button.click()
+        try:
+            WebDriverWait(driver, 4).until(EC.alert_is_present())
+        except NoAlertPresentException:
+            driver.switch_to.window(home)
+            wait.until(EC.visibility_of_element_located((By.ID, 'psbIFrame')))
+            #     # Switch to the frame //Switch to base frame  switchTo().defaultContent()
+            driver.switch_to.frame('psbIFrame')
+            # edit_usage_page = driver.find_elements(By.XPATH, "//span[contains(text(),'Edit Usage Attributes')]")
+            qty_input_new = wait.until(EC.visibility_of_element_located((By.XPATH,
+                                                                         "//label[contains(text(),'*Quantity')]/../../following-sibling::td[1]//input")))  # complex locate element xpath
+            qty_input_new.clear()
+            qty_input_new.send_keys(cqty)
+            time.sleep(1)
+            tech_new = wait.until(EC.visibility_of_element_located((By.XPATH,
+                                                                    "//label[contains(text(),'*Technical')]/../../following-sibling::td[1]//input")))
+            tech_new.clear()
+            tech_new.send_keys(ctechpart)
+            time.sleep(1)
+            driver.find_element(By.XPATH, "//button[contains(text(),'OK')]").click()
+        else:
+            driver.switch_to.alert.accept()
+            driver.find_element(By.ID, 'ext-gen41').click()
+            driver.switch_to.window(home)
+            wait.until(EC.visibility_of_element_located((By.ID, 'psbIFrame')))
+            #     # Switch to the frame //Switch to base frame  switchTo().defaultContent()
+            driver.switch_to.frame('psbIFrame')
+            existpage(driver, wait, parentnumber, cnumber, cqty, ctechpart)
 
 
 # for insert exist items
@@ -156,7 +152,6 @@ def existpage(driver, wait, parentnumbere, number, eqty, etechpart):
         # Can not use element_to_be_clickable, have to use visibility_of_element_located
     wait.until(EC.visibility_of_element_located(
         (By.XPATH, "//*[@class='x-grid3-scroller']//span[contains(text(),'" + parentnumbere + "')]")))
-    insert_exist_button = driver.find_element(By.XPATH, "//button[contains(text(),'Insert Existing')]")
 
     # #enable_state = insert_exist_button.get_attribute("aria-disabled")    get_attribute does not work, do not know why.
     # enable_state = insert_exist_button.is_enabled()
@@ -168,66 +163,71 @@ def existpage(driver, wait, parentnumbere, number, eqty, etechpart):
     # print(str(new_button_state))
     if new_button_state == False:
         return
-    insert_exist_button.click()
+    else:
+        insert_exist_button = wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Insert Existing')]")))
+        insert_exist_button.click()
 
-    # existframe = wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ext-gen1']/iframe[2]")))
-    # existframe = driver.find_element(By.XPATH, "//*[@id='ext-gen1']/iframe[2]")
-    # driver.switch_to.frame(existframe)
-    numberex = wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//label[contains(text(),'Number:')]/following-sibling::div//input")))
-    # numberex = driver.find_element(By.XPATH, "//label[contains(text(),'Number:')]/following-sibling::div//input")
-    numberex.send_keys(number)
-    time.sleep(1)
-    searchbutton = driver.find_element(By.XPATH, "//button[contains(text(),'Search')]")
-    searchbutton.click()
-    time.sleep(1)
-    try:
-        ok_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'OK')]")))
-        # okbutton = driver.find_element(By.XPATH, "//button[contains(text(),'OK')]")
-        ok_button.click()
-    except ElementClickInterceptedException:
+        # existframe = wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ext-gen1']/iframe[2]")))
+        # existframe = driver.find_element(By.XPATH, "//*[@id='ext-gen1']/iframe[2]")
+        # driver.switch_to.frame(existframe)
+        numberex = wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, "//label[contains(text(),'Number:')]/following-sibling::div//input")))
+        # numberex = driver.find_element(By.XPATH, "//label[contains(text(),'Number:')]/following-sibling::div//input")
+        numberex.send_keys(number)
+        time.sleep(1)
+        searchbutton = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Search')]")))
         searchbutton.click()
         time.sleep(1)
-        ok_button.click()
-    # action_chains = ActionChains(driver)
-    # action_chains.double_click(ok_button).perform()
-    time.sleep(2)
-    add_qty_page = driver.find_elements(By.XPATH, "//span[contains(text(),'Edit Usage Attributes')]")
-    if len(add_qty_page) == 1:
-        qty_input = driver.find_element(By.XPATH,
-                                        "//label[contains(text(),'*Quantity')]/../../following-sibling::td[1]//input")  # loacte two same input fields
-        qty_input.clear()
-        qty_input.send_keys(eqty)
-        time.sleep(1)
-        tech = driver.find_element(By.XPATH,
-                                   "//label[contains(text(),'*Technical')]/../../following-sibling::td[1]//input")
-        tech.clear()
-        tech.send_keys(etechpart)
-        time.sleep(1)
-        driver.find_element(By.XPATH, "//button[contains(text(),'OK')]").click()
-    time.sleep(2)
-    confirm_no = driver.find_elements(By.XPATH, "//button[contains(text(),'No')]")
-    if len(confirm_no) == 1:
-        confirm_no_button = WebDriverWait(driver, 4).until(
-            EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'No')]")))
-        confirm_no_button.click()
-    # Edit Usage Attributes page
-    # editusageframe = wait.until(
-    #     EC.visibility_of_element_located((By.XPATH, "//iframe[@class='ext-shim x-ignore']")))
-    # driver.switch_to.frame(editusageframe)
-    #     two_inputs = driver.find_elements(By.XPATH, "//input[@role='spinbutton']")  # loacte two same input fields
-    #     two_inputs[1].send_keys(cqty)
-    #     driver.find_element(By.XPATH, "//*[@role='alert']/preceding-sibling::div//input").send_keys(
-    #         etechpart)
-    #     driver.find_element(By.XPATH, "//button[contains(text(),'OK')]").click()
-    # except Exception:
-    #     pass
-    # wait.until(
-    #     EC.visibility_of_element_located((By.ID, 'psbIFrame'))
-    # )
-    # Switch to the frame //Switch to base frame  switchTo().defaultContent()
-    # //*[@class="x-tree3-node-text"] to locate the items in the Identity
-    # driver.switch_to.frame("psbIFrame")
+        try:
+            ok_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'OK')]")))
+            # okbutton = driver.find_element(By.XPATH, "//button[contains(text(),'OK')]")
+            ok_button.click()
+        except ElementClickInterceptedException:
+            searchbutton.click()
+            time.sleep(1)
+            ok_button.click()
+        # action_chains = ActionChains(driver)
+        # action_chains.double_click(ok_button).perform()
+        time.sleep(2)
+        add_qty_page = driver.find_elements(By.XPATH, "//span[contains(text(),'Edit Usage Attributes')]")
+        if len(add_qty_page) == 1:
+            qty_input = wait.until(EC.visibility_of_element_located((By.XPATH,
+                                                                     "//label[contains(text(),'*Quantity')]/../../following-sibling::td[1]//input")))  # loacte two same input fields
+            qty_input.clear()
+            qty_input.send_keys(eqty)
+            time.sleep(1)
+            tech = wait.until(EC.visibility_of_element_located((By.XPATH,
+                                                                "//label[contains(text(),'*Technical')]/../../following-sibling::td[1]//input")))
+            tech.clear()
+            tech.send_keys(etechpart)
+            time.sleep(1)
+            driver.find_element(By.XPATH, "//button[contains(text(),'OK')]").click()
+        time.sleep(2)
+        confirm_no = driver.find_elements(By.XPATH, "//button[contains(text(),'No')]")
+        if len(confirm_no) == 1:
+            confirm_no_button = WebDriverWait(driver, 4).until(
+                EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'No')]")))
+            confirm_no_button.click()
+        # return False
+        # Edit Usage Attributes page
+        # editusageframe = wait.until(
+        #     EC.visibility_of_element_located((By.XPATH, "//iframe[@class='ext-shim x-ignore']")))
+        # driver.switch_to.frame(editusageframe)
+        #     two_inputs = driver.find_elements(By.XPATH, "//input[@role='spinbutton']")  # loacte two same input fields
+        #     two_inputs[1].send_keys(cqty)
+        #     driver.find_element(By.XPATH, "//*[@role='alert']/preceding-sibling::div//input").send_keys(
+        #         etechpart)
+        #     driver.find_element(By.XPATH, "//button[contains(text(),'OK')]").click()
+        # except Exception:
+        #     pass
+        # wait.until(
+        #     EC.visibility_of_element_located((By.ID, 'psbIFrame'))
+        # )
+        # Switch to the frame //Switch to base frame  switchTo().defaultContent()
+        # //*[@class="x-tree3-node-text"] to locate the items in the Identity
+        # driver.switch_to.frame("psbIFrame")
 
 
 def getfile():
@@ -256,6 +256,7 @@ def all_level(driver):
 
 
 def main():
+    # flag = False
     mdriver, mwait, mhome = initdriver()
     filepath = getfile()
     with open(filepath, 'rb') as f:
@@ -318,7 +319,8 @@ def main():
                                     data['Revision'][i], data['Accessory'][i],
                                     data['Spare\n Part'][i],
                                     data['Gathering Part'][i],
-                                    data['Critical Characteristic'][i], data['Qty'][i], data['Technical Part'][i])
+                                    data['Critical Characteristic'][i], data['Qty'][i],
+                                    data['Technical Part'][i])
                         break
             elif int(data['Level'][i]) < int(data['Level'][i - 1]):
                 # print(i, "aaaa")
@@ -331,13 +333,15 @@ def main():
                                       data['Technical Part'][i])
                         else:
                             # run the insert new page, i是当前行，jj是父行
-                            newpage(mdriver, mwait, mhome, data['Number'][jj], data['Number'][i], data['Name'][i],
+                            newpage(mdriver, mwait, mhome, data['Number'][jj], data['Number'][i],
+                                    data['Name'][i],
                                     data['Assembly Mode'][i].lower(),
                                     data['Source'][i].lower(),
                                     data['Revision'][i], data['Accessory'][i],
                                     data['Spare\n Part'][i],
                                     data['Gathering Part'][i],
-                                    data['Critical Characteristic'][i], data['Qty'][i], data['Technical Part'][i])
+                                    data['Critical Characteristic'][i], data['Qty'][i],
+                                    data['Technical Part'][i])
                         break
 
 
